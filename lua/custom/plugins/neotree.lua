b9376787dc54f876bf9d8441627fe408716b7169
Sofia Relvão <sophieisqueer@protonmail.com>
2023-05-28 19:29:52 +0100

    fix neotree symbols

--- lua/custom/plugins/neotree.lua
+++ lua/custom/plugins/neotree.lua
@@ -39,6 +39,28 @@ return {
 				"git_status",
 				"document_symbols",
 			},
+
+			document_symbols = {
+				kinds = {
+					File = { icon = "󰈙 ", hl = "Tag" },
+					Namespace = { icon = "󰌗 ", hl = "Include" },
+					Package = { icon = "󰏖 ", hl = "Label" },
+					Class = { icon = "󰌗 ", hl = "Include" },
+					Property = { icon = "󰆧 ", hl = "@property" },
+					Enum = { icon = "󰒻 ", hl = "@number" },
+					Function = { icon = "󰊕 ", hl = "Function" },
+					String = { icon = "󰀬 ", hl = "String" },
+					Number = { icon = "󰎠 ", hl = "Number" },
+					Array = { icon = "󰅪 ", hl = "Type" },
+					Object = { icon = "󰅩 ", hl = "Type" },
+					Key = { icon = "󰌋 ", hl = "" },
+					Struct = { icon = "󰌗 ", hl = "Type" },
+					Operator = { icon = "󰆕 ", hl = "Operator" },
+					TypeParameter = { icon = "󰊄 ", hl = "Type" },
+					StaticMethod = { icon = '󰠄 ', hl = 'Function' },
+				}
+			},
+
 			source_selector = {
 				winbar = false,
 				statusbar = false,
