f134ffd491b6639788c2ce857b3577b23da1b144
Sofia Relvão <sophieisqueer@protonmail.com>
2023-05-28 18:45:42 +0100

    Barbecue config

--- init.lua
+++ init.lua
@@ -367,7 +367,7 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
-local on_attach = function(_, bufnr)
+local on_attach = function(client, bufnr)
   -- NOTE: Remember that lua is a real programming language, and as such it is possible
   -- to define small helper and utility functions so you don't have to repeat yourself
   -- many times.
@@ -408,6 +408,11 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
+
+  -- Get nvim-navic working with multiple tabs (barbecue.nvim)
+  if client.server_capabilities["documentSymbolProvider"] then
+    require("nvim-navic").attach(client, bufnr)
+  end
 end
 
 -- Enable the following language servers
