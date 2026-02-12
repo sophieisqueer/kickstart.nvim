9ce65159da527e205834ff1f21cf33a52d204061
Sofia Relvão <sophieisqueer@protonmail.com>
2026-02-12 22:09:04 +0000

    fedora sync

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,88 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'lervag/vimtex',
+    ft = { 'tex', 'markdown' },
+    lazy = false,
+    init = function()
+      vim.g.vimtex_view_method = 'general'
+      vim.g.vimtex_view_general_viewer = 'papers'
+      -- Config para math em markdown
+      vim.g.vimtex_syntax_conceal = {
+        accents = 0,
+        cites = 0,
+        fancy = 0,
+        greek = 0,
+        math_bounds = 0,
+        math_delimiters = 0,
+        math_fracs = 0,
+        math_super_sub = 0,
+        math_symbols = 0,
+        sections = 0,
+        styles = 0,
+      }
+    end,
+  },
+  {
+    dir = '/home/phi/Code/peek.nvim',
+    event = { 'VeryLazy' },
+    build = 'deno task --quiet build:fast',
+    config = function()
+      require('peek').setup {
+        auto_load = true,
+        --
+        -- app = {
+        --   'chromium-browser',
+        --   '--incognito',
+        --   '--noerrdialogs',
+        --   '--disable-translate',
+        --   '--disable-cache',
+        --   '--disk-cache-dir=/dev/null',
+        --   '--disk-cache-size=1',
+        --   '--app=http://localhost:8888/?theme=dark',
+        -- },
+      }
+      vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
+      vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
+    end,
+  },
+  {
+    'folke/trouble.nvim',
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = 'Trouble',
+    keys = {
+      {
+        '<leader>xx',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Diagnostics (Trouble)',
+      },
+      {
+        '<leader>xX',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+      {
+        '<leader>cs',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = 'Symbols (Trouble)',
+      },
+      {
+        '<leader>cl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>xL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = 'Location List (Trouble)',
+      },
+      {
+        '<leader>xQ',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = 'Quickfix List (Trouble)',
+      },
+    },
+  },
+}
